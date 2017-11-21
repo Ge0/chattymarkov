e9fba121cffe1e34fb08c8073d44d9c994b246d6
Geoffrey ROYER <geoffrey.royer@gmail.com>
2017-11-21 18:11:36 +0100

    Build redis database through query string

--- chattymarkov/database/__init__.py
+++ chattymarkov/database/__init__.py
@@ -2,8 +2,111 @@
 
 This submodule gathers all the supported database formats.
 """
-
 # flake8: noqa
 from .json import JSONFileDatabase
 from .memory import MemoryDatabase
 from .redis import RedisDatabase
+
+
+class ChattymarkovDatabaseError(Exception):
+    """Base exception class for chattymarkov.database related errors."""
+
+
+class UnknownDatabasePrefixerror(ChattymarkovDatabaseError):
+    """Exception class for unknown database prefixes errors."""
+
+
+class InvalidConnectionStringError(ChattymarkovDatabaseError):
+    """Exception class for invalid connection string error."""
+
+
+_DATABASE_PREFIXES = {}
+
+
+def database(prefix):
+    """Wrap a function responsible for building a database."""
+
+    def wrapper(func):
+        """Register `func` in the global `_DATABASE_PREFIXES` hash."""
+        _DATABASE_PREFIXES[prefix] = func
+        return func
+    return wrapper
+
+
+def get_database_builder(prefix):
+    """Get the function associated to `prefix` to instanciate a database.
+    
+    This function is a simple interface around the `_DATABASE_PREFIXES` hash.
+
+    Args:
+        prefix (str): the prefix's database function.
+
+    Raises:
+        UnknownDatabasePrefixError: the prefix is not recognized.
+
+    Returns:
+        function: the function assiociated to the `prefix`.
+    """
+    if prefix not in _DATABASE_PREFIXES:
+        raise UnknownDatabasePrefixError(
+            "Database prefix '{}' is unknown.".format(prefix))
+    return _DATABASE_PREFIXES[prefix]
+
+
+@database('redis')
+def build_redis_database(resource):
+    """Build a `RedisDatabase` instance to communicate with a redis server.
+
+    Args:
+        resource (str): a string that represents connection information.
+
+    Returns:
+        RedisDatabase: instance to communicate with the redis server.
+    """
+    whitelist = {'db'}
+    extra_params = {}
+
+    connection, *params = resource.split(';')
+
+    # Parse additional parameters, if any
+    if len(params) > 0:
+        for param in params:
+            key, equal, value = param.partition('=')
+            if key in whitelist:
+                extra_params[key] = value
+
+    if connection.startswith('/'):
+        # UNIX socket connection
+        return RedisDatabase(unix_socket_path=connection,
+                             **extra_params)
+    else:
+        # TCP socket connection
+        host, colon, port = connection.partition(':')
+        if host != '' and colon == ':' and port.isnumeric():
+            return RedisDatabase(host=host, port=int(port),
+                                 **extra_params)
+
+
+def build_database_connection(connect_string):
+    """Build a database connection based on `connect_string`.
+
+    Args:
+        connect_string (str): connection string for the database connection.
+
+    Raises:
+        InvalidConnectionStringError: raised when the `connect_string` is
+            invalid.
+        UnknownDatabasePrefixError: raised when the database prefix is
+            unknown.
+    Returns:
+        AbstractDatabase: an instance of AbstractDatabase that handle a
+            connection to the desired database.
+    """
+    prefix, colon_slash_slash, resource = connect_string.partition('://')
+    if resource != '':
+        builder = get_database_builder(prefix)
+        return builder(resource)
+    else:
+        raise InvalidConnectionStringError(
+            "Invalid connection string '{}'. Must be of the form "
+            "prefix://resource;param1=value1;param2=value2...")
--- chattymarkov/database/redis.py
+++ chattymarkov/database/redis.py
@@ -9,12 +9,33 @@ from .base import AbstractDatabase
 class RedisDatabase(AbstractDatabase):
     def __init__(self, host="localhost", port=6739, db=0,
                  unix_socket_path=None):
+
+        self._db = int(db)
+
         if unix_socket_path is not None:
-            self.handle = redis.StrictRedis(
-                unix_socket_path=unix_socket_path, db=db
-            )
+            self._unix_socket_path = unix_socket_path
+            self.handle = redis.StrictRedis(unix_socket_path=unix_socket_path,
+                                            db=db)
         else:
+            self._host = host
+            self._port = int(port)
             self.handle = redis.StrictRedis(host=host, port=port, db=db)
+
+    @property
+    def host(self):
+        return self._host
+
+    @property
+    def port(self):
+        return self._port
+
+    @property
+    def db(self):
+        return self._db
+
+    @property
+    def unix_socket_path(self):
+        return self._unix_socket_path
 
     def add(self, key, element):
         return self.handle.sadd(key, element.encode()) > 0
