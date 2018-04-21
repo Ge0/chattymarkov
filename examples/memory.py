752598c66c2d73cf916b09e6de36a21e1eecf241
Geoffrey ROYER <geoffrey.royer@gmail.com>
2018-04-21 15:51:54 +0200

    Isortize examples

--- examples/memory.py
+++ examples/memory.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from chattymarkov import ChattyMarkov
 
+
 markov = ChattyMarkov("memory://")
 markov.learn("My favorite animal is the crocodile")
 markov.learn("The word animal is six letters long")
